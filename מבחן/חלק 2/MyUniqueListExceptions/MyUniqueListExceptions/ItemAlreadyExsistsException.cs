4cb66238a0e451c0efe1b3632e6e9aa70b0bcdcd
amity94 <45635119+amity94@users.noreply.github.com>
2019-05-19 14:18:33 +0300

    Add files via upload

---
+++ מבחן/חלק 2/MyUniqueListExceptions/MyUniqueListExceptions/ItemAlreadyExsistsException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SecretAgent
+{
+    [Serializable]
+    internal class ItemAlreadyExsistsException : Exception
+    {
+        public ItemAlreadyExsistsException()
+        {
+        }
+
+        public ItemAlreadyExsistsException(string message) : base(message)
+        {
+        }
+
+        public ItemAlreadyExsistsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ItemAlreadyExsistsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ מבחן/חלק 2/MyUniqueListExceptions/MyUniqueListExceptions/MyUniqueList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretAgent
+{
+
+    class MyUniqueList
+    {
+        List<int> list = new List<int>();
+
+        public MyUniqueList()
+        {
+         
+        }
+
+        public bool Add(int item)
+        {
+            if (!list.Contains(item))
+            {
+                list.Add(item);
+                return true;
+            }
+
+            if(list.Contains(item))
+            {
+                throw new ItemAlreadyExsistsException();
+            }
+
+            return false;
+        }
+
+        public bool Remove(int item)
+        {
+            if (list.Contains(item))
+            {
+                list.Remove(item);
+                return true;
+            }
+            if(!list.Contains(item))
+            {
+                throw new ItemNotFoundException();
+            }
+            return false;
+        }
+
+        public int Peek(int index)
+        {
+            if(index > list.Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return list[index];
+
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return this.list[index];
+            }
+            set
+            {
+                if (list[index] == value)
+                    return;
+                if (list.Contains(value))
+                    return;
+                list[index] = value;
+            }
+        }
+    }
+}
